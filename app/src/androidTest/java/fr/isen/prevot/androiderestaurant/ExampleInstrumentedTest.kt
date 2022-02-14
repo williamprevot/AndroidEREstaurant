f45f4c12fe669ec1295a770854bb3fedf63ed106
williamprevot <guy-william.prevot@isen.yncrea.fr>
2022-02-14 17:00:10 +0100

    apk genereted

--- app/src/androidTest/java/fr/isen/prevot/androiderestaurant/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/fr/isen/prevot/androiderestaurant/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("fr.isen.janowski.androiderestaurant", appContext.packageName)
+        assertEquals("fr.isen.prevot.androiderestaurant", appContext.packageName)
     }
 }
